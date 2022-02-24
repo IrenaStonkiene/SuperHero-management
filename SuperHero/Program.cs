697322422002458bb49924d08e5e4ae234981bb7
IrenaStonkiene <irena.stonkiene@hotmail.com>
2022-02-24 21:09:25 +0200

    New SuperHero script

--- SuperHero/Program.cs
+++ SuperHero/Program.cs
@@ -9,7 +9,30 @@ namespace SuperHero
     internal class Program
     {
         static void Main(string[] args)
+          
+
         {
+            string name = "Spider"
+            int age = 100
+            float hight = 1.9f
+            char gender = "M"
+            string heroPower1, heroPower2, heroPower3
+            heroPower1 = "Can jump";
+            heroPower2 = "Knows 20 languages";
+            heroPower3 = "Can be invisable";
+
+
+            Console.WriteLine("**************************************");
+            Console.WriteLine($"Hero: {name}");
+            Console.WriteLine("Age: " + age");
+            Console.WriteLine("hight");
+            Console.WriteLine("gender");
+            Console.WriteLine("Hero power: ")
+            Console.WriteLine("**************************************");
+
+
+
+
         }
     }
 }
