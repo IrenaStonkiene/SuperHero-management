158f486bef344884270b374b1c31dc39e795f506
IrenaStonkiene <irena.stonkiene@hotmail.com>
2022-03-08 20:21:39 +0200

    Cookie calculation version 2

--- SuperHero/Program.cs
+++ SuperHero/Program.cs
@@ -9,13 +9,11 @@ namespace SuperHero
     internal class Program
     {
         static void Main(string[] args)
-          
-
         {
             string name = "Spider";
             int age = 100;
             float hight = 1.9f;
-            char gender = "M";
+            char gender = 'M';
             string heroPower1, heroPower2, heroPower3;
             heroPower1 = "Can jump";
             heroPower2 = "Knows 20 languages";
@@ -30,8 +28,8 @@ namespace SuperHero
             Console.WriteLine("**************************************");
             Console.WriteLine($"Hero: {name}");
             Console.WriteLine($"Age: {age}");
-            Console.WriteLine($"hight: {hight}");
-            Console.WriteLine($"gender: {gender}");
+            Console.WriteLine($"Hight: {hight}");
+            Console.WriteLine($"Gender: {gender}");
             Console.WriteLine("**************************************");
 
 
@@ -40,17 +38,23 @@ namespace SuperHero
             Console.WriteLine("*************Additional info************************");
 
             var TimeonDeed = deedTimeInHours1 + deedTimeInHours2 + deedTimeInHours3;
-            Console.WriteLine($"Spider spends total hours on deeds: {TimeonDeed}");
+            Console.WriteLine($"Spider spends total {TimeonDeed} hours on deeds");
 
             var monthlySalary= TimeonDeed * cookieCost;
-            Console.WriteLine($"Spider earns Cookies: {monthlySalary}");
+            Console.WriteLine($"Our hero earns {monthlySalary} cookies per month");
             
             var AverageTimeonDeed = TimeonDeed / 3;
-            Console.WriteLine($"Spider spends avergae time on deed is: {AverageTimeonDeed}");
+            Console.WriteLine($"He spends average {AverageTimeonDeed} hours on deeds");
 
             var boughtCookies = monthlySalary / cookieCost;
-            Console.WriteLine($"Spider can buy {boughtCookies}");
+            Console.WriteLine($"Spider can buy {boughtCookies} cookies");
             
             if (!isEvil)
-            {Console.WriteLine("Spider protects the city")};
+            {
+                Console.WriteLine("Spider is not an evil, he protects the city");
+            }
             Console.WriteLine("*******************************************************");
+            Console.ReadLine();
+        }
+    }
+}
